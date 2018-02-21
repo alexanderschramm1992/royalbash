b63e4e94e2b085305792439b84baaea716ed26f2
Alexander Schramm <alexander.schramm1992@gmail.com>
2018-02-21 20:48:48 +0100

    Fiddling with React components in parameters WIP

--- app/index.tsx
+++ app/index.tsx
@@ -2,19 +2,44 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import Creature from "./components/card/Creature";
+import CardContainer from "./components/board/CardContainer";
+
+const cards: Creature[] = new Array<Creature>(
+  new Creature({
+    id: "1",
+    name: "Lorem Ipsum",
+    image: "N/A",
+    type: "Creature",
+    subType: "Lorem Ipsum",
+    text: "N/A",
+    lore: "N/A",
+    cost: 99,
+    strength: 99,
+    health: 99
+  })
+);
+
+/*
+<Creature
+    id = "1"
+    name = "Lorem Ipsum"
+    image = "N/A"
+    type = "Creature"
+    subType = "Lorem Ipsum"
+    text = "N/A"
+    lore = "N/A"
+    cost = {99}
+    strength = {99}
+    health = {99}
+/>
+*/
 
 ReactDOM.render(
-    <Creature
-        id = "1"
-        name = "Lorem Ipsum"
-        image = "N/A"
-        type = "Creature"
-        subType = "Lorem Ipsum"
-        text = "N/A"
-        lore = "N/A"
-        cost = {99}
-        strength = {99}
-        health = {99}
-    />,
+    <div>
+      <CardContainer
+        cards = {cards}
+      />
+    </div>
+    ,
     document.getElementById("app")
 );
