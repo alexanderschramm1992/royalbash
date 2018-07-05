327fadc8dbadbb53352f524e93b510c91890f1f4
Alexander Schramm <alexander.al.schramm@deutschebahn.com>
2018-07-05 08:48:27 +0200

    Added new summoning card

--- src/main/java/de/schramm/royalbash/data/SummoningDeckData.java
+++ src/main/java/de/schramm/royalbash/data/SummoningDeckData.java
@@ -1,9 +1,7 @@
 package de.schramm.royalbash.data;
 
 import de.schramm.royalbash.gameengine.model.SummoningDeck;
-import de.schramm.royalbash.gameengine.model.card.summoningcard.BattlefieldMedic;
-import de.schramm.royalbash.gameengine.model.card.summoningcard.EagerCadet;
-import de.schramm.royalbash.gameengine.model.card.summoningcard.YouthfulKnight;
+import de.schramm.royalbash.gameengine.model.card.summoningcard.*;
 
 import java.util.UUID;
 
@@ -11,6 +9,8 @@ class SummoningDeckData {
 
     static final SummoningDeck SUMMONING_DECK_1 = SummoningDeck.builder()
             .id(UUID.fromString("b68ba1e2-c8bf-470d-8172-ef1632482fe8"))
+            .card(new PlatedTuskrider())
+            .card(new ProvenCombatant())
             .card(new BattlefieldMedic())
             .card(new EagerCadet())
             .build();
