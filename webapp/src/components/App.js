056cee021740aa996788d49c05048dfdb97c5e96
Alexander Schramm <alexander.schramm@maibornwolff.de>
2019-01-07 18:23:05 +0100

    Redesign NPM infrastructure

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import logo from "../images/logo.svg";
-import "./App.css";
 import GamePicker from "./GamePicker";
 import Board from "./Board";
 
@@ -17,10 +15,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         {!this.state.gameLoaded &&
             <GamePicker store={this.props.store}/>
         }
