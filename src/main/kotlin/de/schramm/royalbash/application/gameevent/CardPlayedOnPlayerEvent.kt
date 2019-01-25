b4a4fa3682664a5660ec7af5ff3baaf33a86300e
Alexander Schramm <alexander.schramm@maibornwolff.de>
2019-01-25 20:52:22 +0100

    Increasing readability more (hopefully)

--- src/main/kotlin/de/schramm/royalbash/application/gameevent/CardPlayedOnPlayerEvent.kt
+++ src/main/kotlin/de/schramm/royalbash/application/gameevent/CardPlayedOnPlayerEvent.kt
@@ -1,23 +1,28 @@
 package de.schramm.royalbash.application.gameevent
 
+import de.schramm.royalbash.domain.Card
 import de.schramm.royalbash.domain.Game
-import java.util.*
+import de.schramm.royalbash.domain.Player
 
-data class CardPlayedOnPlayerEvent(
-        val cardId: String,
-        val ownerId: String,
-        val targetPlayerId: String
-) : GameEvent {
+data class CardPlayedOnPlayerEvent(val cardId: String, val ownerId: String, val targetPlayerId: String): GameEvent {
 
     constructor(): this(cardId = "", ownerId = "", targetPlayerId = "")
 
     override fun invoke(game: Game): Game {
         return game.findPlayer(ownerId)
-                ?.let { Optional.ofNullable(it.findHandcard(cardId))
-                            .map { card -> game.findPlayer(targetPlayerId)
-                                    ?.let { targetPlayer -> game.playCard(card, it, targetPlayer) }
-                                    ?:game }
-                            .orElse(game) }
+                ?.let { invokeWith(it, game) }
+                ?: game
+    }
+
+    private fun invokeWith(owner: Player, game: Game): Game {
+        return owner.findHandcard(cardId)
+                ?.let { invokeWith(it, owner, game) }
+                ?: game
+    }
+
+    private fun invokeWith(card: Card, owner: Player, game: Game): Game {
+        return game.findPlayer(targetPlayerId)
+                ?.let { game.playCard(card, owner, it) }
                 ?: game
     }
 }
