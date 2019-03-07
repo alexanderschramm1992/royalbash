8bfed9319a9bf076f1ccc2c05f41a397a1c90a19
Alexander Schramm <alexander.schramm@maibornwolff.de>
2019-03-07 08:14:10 +0100

    Removing Profile (not needed yet)

--- src/main/kotlin/de/schramm/royalbash/infrastructure/InMemoryDatabaseConfig.kt
+++ src/main/kotlin/de/schramm/royalbash/infrastructure/InMemoryDatabaseConfig.kt
@@ -4,9 +4,7 @@ import de.schramm.royalbash.infrastructure.database.GamePersistenceOperations
 import de.schramm.royalbash.infrastructure.database.InMemoryGamePersistenceOperations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
-@Profile("inmemory")
 @Configuration
 open class InMemoryDatabaseConfig {
 
