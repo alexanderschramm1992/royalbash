b5933a2b7bbf4e05da75c402a4f17c0b75263cf1
Alexander Schramm <alexander.schramm@maibornwolff.de>
2019-09-18 21:23:33 +0200

    Removed obsolete file

--- backend/src/main/resources/static/game.js
+++
@@ -1,8 +0,0 @@
-import App from './components/gamepicker/App.js';
-
-new Vue({
-    render: h => h(App),
-    el: '#app',
-    vuetify: new Vuetify(),
-    created: () => this.vuetify.theme.dark = true,
-}).$mount(`#app`);
